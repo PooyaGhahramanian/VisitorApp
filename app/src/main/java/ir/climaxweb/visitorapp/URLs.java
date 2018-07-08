4458d0b3b8bbb01f9b5a1308d72720b361195a43
PooyaGhahramanian <PooyaGhahramanian@Gmail.com>
2018-07-08 08:38:34 +0430

    Login activity Done + added all files

---
+++ app/src/main/java/ir/climaxweb/visitorapp/URLs.java
@@ -0,0 +1,11 @@
+package ir.climaxweb.visitorapp;
+
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://se.climaxweb.ir/api/";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup/";
+    public static final String URL_LOGIN= ROOT_URL + "login/?";
+
+}
---
+++ app/src/main/java/ir/climaxweb/visitorapp/User.java
@@ -0,0 +1,23 @@
+package ir.climaxweb.visitorapp;
+
+//this is very simple class and it only contains the user attributes, a constructor and the getters
+// you can easily do this by right click -> generate -> constructor and getters
+public class User {
+
+    private int id;
+    private String email;
+
+    public User(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
